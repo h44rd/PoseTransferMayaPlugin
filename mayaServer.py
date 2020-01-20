2dd54df8b8e19d099ef383fe5e988c4b7c9b4145
Haard Panchal <panchalhaard@gmail.com>
2020-01-20 13:02:10 -0600

    Milestone reached

--- mayaServer.py
+++ mayaServer.py
@@ -11,32 +11,33 @@ import time
 import sys
 from multiprocessing.connection import Listener
 
-address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
-listener = Listener(address, authkey='secret password'.encode())
-conn = listener.accept()
-print('connection accepted from', listener.last_accepted)
-while True:
-    msg = conn.recv()
-    # do something with msg
-    print(msg)
-    conn.send("hello from the other side...")
-    if msg == 'close':
-        conn.close()
-        break
+# address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
+# listener = Listener(address, authkey='secret password'.encode())
+# conn = listener.accept()
+# print('connection accepted from', listener.last_accepted)
+# while True:
+#     msg = conn.recv()
+#     # do something with msg
+#     print(msg)
+#     conn.send("hello from the other side...")
+#     if msg == 'close':
+#         conn.close()
+#         break
+# listener.close()
 
-cap = cv2.VideoCapture(VIDEO_FILE_PATH)
+# cap = cv2.VideoCapture(VIDEO_FILE_PATH)
 
-listener.close()
+
 
 class Server:
     def __init__(self):
         self.DIR_PATH = dirname(realpath(__file__))
-        self.PROJECT_PATH = realpath(DIR_PATH + '/..')
-        self.IMAGE_FILE_PATH = PROJECT_PATH + '/data/images/test_image.png'
-        self.VIDEO_FILE_PATH = PROJECT_PATH + '/data/videos/test.mp4'
-        self.SAVED_SESSIONS_DIR = PROJECT_PATH + '/data/saved_sessions'
-        self.SESSION_PATH = SAVED_SESSIONS_DIR + '/init_session/init'
-        self.PROB_MODEL_PATH = SAVED_SESSIONS_DIR + '/prob_model/prob_model_params.mat'
+        self.PROJECT_PATH = realpath(self.DIR_PATH + '/..')
+        self.IMAGE_FILE_PATH = self.PROJECT_PATH + '/data/images/test_image.png'
+        self.VIDEO_FILE_PATH = self.PROJECT_PATH + '/data/videos/test.mp4'
+        self.SAVED_SESSIONS_DIR = self.PROJECT_PATH + '/data/saved_sessions'
+        self.SESSION_PATH = self.SAVED_SESSIONS_DIR + '/init_session/init'
+        self.PROB_MODEL_PATH = self.SAVED_SESSIONS_DIR + '/prob_model/prob_model_params.mat'
 
         self.address = ('localhost', 6000)
 
@@ -49,10 +50,10 @@ class Server:
         if not self.cap.isOpened():
             raise IOError("Cannot open video")
         
-        temp_image = self.cap.read()
+        hasFrame, temp_image = self.cap.read()
         self.image_size = temp_image.shape
 
-        self.pose_estimator = PoseEstimator(self.image_size, SESSION_PATH, PROB_MODEL_PATH)
+        self.pose_estimator = PoseEstimator(self.image_size, self.SESSION_PATH, self.PROB_MODEL_PATH)
         self.pose_estimator.initialise()
 
         self.processRate = 10 # Pose will be estimated for every self.processRate frames
@@ -70,7 +71,7 @@ class Server:
         while hasFrame:
             try:
                 pose_2d, visibility, pose_3d = self.processFrame(image)
-                self.sendObjs(pose_3d)
+                self.sendObjs(pose_3d.tolist())
             except ValueError:
                 print("No visible people in the image.")
                 self.sendObjs("No visible people in the image.")
@@ -82,3 +83,5 @@ class Server:
         pose_2d, visibility, pose_3d = self.pose_estimator.estimate(image)
         return pose_2d, visibility, pose_3d
 
+s = Server()
+s.processVideo()
--- poseTransfer.py
+++ poseTransfer.py
@@ -1,6 +1,7 @@
 import sys
 import maya.OpenMaya as OpenMaya
 import maya.OpenMayaMPx as OpenMayaMPx
+import maya.cmds as cmds
 
 from multiprocessing.connection import Client
 
@@ -11,15 +12,43 @@ class scriptedCommand(OpenMayaMPx.MPxCommand):
     def __init__(self):
         OpenMayaMPx.MPxCommand.__init__(self)
         self.address = ('localhost', 6000)
-        self.conn = Client(self.address, authkey='secret password')        
+        self.conn = Client(self.address, authkey='secret password')
+        self.ifSpheresConstructed = False
+        self.timeFrame = 1
+        self.timeFrameIncrement = 4
+        self.surfaceObjects = []
         # self.conn.send('Connected!')
+
     # Invoked when the command is run.
 
     def doIt(self,argList):
-        print "Hello World!"
+        print "In doIt"
         while True:
             msg = self.conn.recv()
             print "Received message: ", msg
+            print "Number of spheres", len(msg[0][0])
+
+            if not self.ifSpheresConstructed: # To construct the sphere after the first message is received
+                for i in range(len(msg[0][0])):
+                    print "Creating ", 'sphere' + str(i) 
+                    name = cmds.sphere(r = 100)
+                    print name
+                    self.surfaceObjects.append(name[0])
+                self.ifSpheresConstructed = True
+            
+            for i in range(len(msg[0][0])):
+                sphereName = self.surfaceObjects[i]
+
+                x = msg[0][0][i]
+                y = msg[0][1][i]
+                z = msg[0][2][i]
+                
+                cmds.move(x, y, z, sphereName, absolute=True)
+                cmds.setKeyframe(sphereName, time = self.timeFrame)
+            
+            self.timeFrame = self.timeFrame + self.timeFrameIncrement
+
+                
 
 # Creator
 def cmdCreator():
